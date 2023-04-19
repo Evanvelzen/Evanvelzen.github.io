0fdafb673f0b176d13806e8f04ec54802539e754
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2023-04-19 09:09:59 +0200

    taalfouten & vertaling
    ik heb de taalfouten verbeterd en de profiel pagina vertaald

--- js/profile.js
+++ js/profile.js
@@ -2,18 +2,20 @@ const drop = document.querySelector(".location")
 
 const profile = {
     emailAdress: "schoolEmail: 302942781@student.rocmondriaan.nl ",
-    username: "username: EvanVelzen",
-    birthdate: "birthdate : 22-05-2003",
-    biography: "i am a person born on the 22nd of may 2003, ",
+    username: "gebruikersnaam: EvanVelzen",
+    birthdate: "verjaardag : 22-05-2003",
+    biography: "ik ben geboren op 22 mei 2003 en getochen in Nederland.",
     projects: [
         {
-            title: "boter,  kaas en eieren",
-            description: "a simple game known as tic tac toe",
+            title: "Boter,  kaas en eieren",
+            description: "Een simpel boter kaas en eieren spelletje.",
+            img:"",
             teamMate: []
         },
         {
             title: "webCatalogus",
-            description: "a web catalogus that displays a number of games",
+            description: "een Webcatalogus over games.",
+            img:"",
             teamMates: [{
                 fullName: "David Aalderink",
                 link: "Daalderink.github.io"
@@ -25,10 +27,10 @@ const profile = {
         },
 
     ],
-    education: "doing currently mbo-4 software development",
+    education: "Ik doe op het moment software development bij het ROC Mondriaan.",
     skills: "fixing pc's",
-    laungauges: ["dutch", "english", "small bit of russian", "small bit of german", "small bit of japanese"],
-    bannerColor: "red",
+    laungauges: ["Nederlands", "Engels", "heel klein beetje Russisch", "beetje Duits", "heel klein beetje Japans zelfs"],
+    bannerColor: "Rood",
     socialMedia: "#",
 }
 
@@ -40,7 +42,7 @@ drop.innerHTML += profile.username + "<br>";
 
 drop.innerHTML += profile.emailAdress + "<br>";
 drop.innerHTML += profile.birthdate + "<br>";
-drop.innerHTML += profile.profilePicture + "<br>";
+// drop.innerHTML += profile.profilePicture + "<br>";
 drop.innerHTML += profile.biography + "<br>";
 drop.innerHTML += profile.laungauges + "<br>";
 drop.innerHTML += profile.skills + "<br>";
@@ -49,8 +51,9 @@ drop.innerHTML += profile.socialMedia + "<br>";
 for (let index = 0; index < profile.projects.length; index++) {
     const projecty = profile.projects[index];
     drop.innerHTML += `<div class="card ">
-
+        <img class="card-img-top" src="">
         <div class="card-body">
+            <
             <h4 class="card-title">${projecty.title}</h4>
             <p class="card-text">${projecty.description}</p>
         </div>
