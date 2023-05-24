f05e6583797a3b25a6ed17156ac4ac5ef79d7f41
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2023-05-24 12:12:46 +0200

    socials toegevoegd
    socials toe gevoegd op het portfolio

--- js/profile.js
+++ js/profile.js
@@ -5,40 +5,48 @@ const profile = {
     username: "Gebruikersnaam: EvanVelzen.",
     birthdate: "Verjaardag : 22-05-2003",
     biography: "Ik ben geboren op 22 mei 2003 en getochen in Nederland.",
-    projects: [
-        {
-            title: "Boter,  kaas en eieren",
-            description: "Een simpel boter kaas en eieren spelletje.",
-            img: "",
-            teamMates: []
-        },
-        {
-            title: "webCatalogus",
-            description: "een Webcatalogus over games.",
-            img: "",
-            teamMates: [{
-                fullName: "David Aalderink",
-                link: "Daalderink.github.io"
-            },
-            {
-                fullName: "Nathen Alberto",
-                link: "Nalberto.github.io"
-            }]
-        },
+    // projects: [
+    //     {
+    //         title: "boter, kaas en eiereren",
+    //         description: "Een simpel boter kaas en eieren spelletje.",
+    //         img: "",
+    //         teamMates: []
+    //     },
+    //     {
+    //         title: "webCatalogus",
+    //         description: "een Webcatalogus over games.",
+    //         img: "",
+    //         teamMates: [{
+    //             fullName: "David Aalderink",
+    //             link: "Daalderink.github.io"
+    //         },
+    //         {
+    //             fullName: "Nathen Alberto",
+    //             link: "Nalberto.github.io"
+    //         }]
+    //     },
 
-    ],
+    // ],
     education: "Ik doe op het moment software development bij het ROC Mondriaan.",
     skills: "Repareren en upgraden van pc's.",
     laungauges: ["Talen die ik spreek", "Nederlands", "Engels", "heel klein beetje Russisch", "beetje Duits", "heel klein beetje Japans zelfs."],
     bannerColor: "Rood",
-    socialMedia: "#",
+    socialMedia: [{
+        title: "Facebook",
+        img: "/img/facebook icon.png",
+        link: "https://www.facebook.com/profile.php?id=100008801014229"
+    }, {
+        title: "Instagram",
+        img: "/img/instagram_icon.png",
+        link: "https://www.instagram.com/emielvanvelzen/"
+    }]
 }
 
 
 
 console.log(profile);
 // personal.innerHTML += profile.profilePicture + "<br>";
-personal.innerHTML += profile.username + "<br>"; 
+personal.innerHTML += profile.username + "<br>";
 
 personal.innerHTML += profile.emailAdress + "<br>";
 personal.innerHTML += profile.birthdate + "<br>";
@@ -48,17 +56,31 @@ personal.innerHTML += profile.laungauges + "<br>";
 personal.innerHTML += profile.skills + "<br>";
 
 
-for (let index = 0; index < profile.projects.length; index++) {
-    const projecty = profile.projects[index];
-    drop.innerHTML += `<div class="col-md-4 mt-3 d-flex align-items-stretch"> <div class="card ">
-        <img class="card-img-top" src="${projecty.img}">
-        <div class="card-body">
-            
-            <h4 class="card-title">${projecty.title}</h4>
-            <p class="card-text">${projecty.description}</p>
-            <a href="${projecty.teamMates}"
+// for (let index = 0; index < profile.projects.length; index++) {
+//     const projecty = profile.projects[index];
+//     drop.innerHTML += `<div class="col-md-4 mt-3 d-flex align-items-stretch"> <div class="card ">
+//         <img class="card-img-top" src="${projecty.img}">
+//         <div class="card-body">
+
+//             <h4 class="card-title">${projecty.title}</h4>
+//             <p class="card-text">${projecty.description}</p>
+//             <a href="${projecty.teamMates}"
+//         </div>
+//         </div>
+//     </div>
+//     `
+// }
+
+for (let index = 0; index < profile.socialMedia.length; index++) {
+    const social = profile.socialMedia[index];
+    drop.innerHTML += `<div class="col-md-3 mt-3 d-flex align-items-stretch"> <div class="card ">
+           <img class="card-img-top" src="${social.img}">
+          <div class="card-body">
+                
+                <h4 class="card-title">${social.title}</h4>
+      
+                <a href="${social.link}">link to social</a>
+            </div>
         </div>
-        </div>
-    </div>
-    `
+       </div>`
 }
