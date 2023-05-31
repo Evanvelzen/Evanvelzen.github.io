08adf9ee0d6129454913993f216ba5d7689d4839
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2023-05-31 10:24:56 +0200

    begin gemaakt aan projecten toevoegen

--- js/projecten.js
+++ js/projecten.js
@@ -1,2 +1,6 @@
 const software = document.querySelector('.software');
 const overign = document.querySelector('.overig');
+
+const projectSoftware = {
+    projecten: [{title: "boter, kaas en eieren"},]
+}
