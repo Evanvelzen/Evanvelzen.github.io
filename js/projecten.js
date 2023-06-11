357537229383ae7145b417dcfb2e75c9ed3bbe6d
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2023-06-11 12:49:13 +0200

    img im port van oud portfolio

--- js/projecten.js
+++ js/projecten.js
@@ -2,5 +2,9 @@ const software = document.querySelector('.software');
 const overign = document.querySelector('.overig');
 
 const projectSoftware = {
-    projecten: [{title: "boter, kaas en eieren"},]
+    projecten: [{   title: "boter, kaas en eieren",
+                    img: "/img/tic tac toe.png",
+                    text: `dit is een digitale versie van het spel boter kaas en eieren. het spel is gemaakt in 2022. het spel heeft het thema van de boeren protesten die er toen waren. het spel zelf detecteerd of er iemand gewonnen heeft. ook laat spel zien wie er aan de beurt is.`,
+                    link: "/software/boter kaas en eieren/index.html",
+},]
 }
