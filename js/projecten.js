b94d765785870a8e9a96e8cb85ecfa8d600888e7
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2023-06-11 15:07:07 +0200

    gedeelte projecten 9in  javascript toe gevoegd

--- js/projecten.js
+++ js/projecten.js
@@ -1,10 +1,19 @@
-const software = document.querySelector('.software');
-const overign = document.querySelector('.overig');
+const software = document.querySelector(".software");
+const overign = document.querySelector(".overig");
 
 const projectSoftware = {
-    projecten: [{   title: "boter, kaas en eieren",
-                    img: "/img/tic tac toe.png",
-                    text: `dit is een digitale versie van het spel boter kaas en eieren. het spel is gemaakt in 2022. het spel heeft het thema van de boeren protesten die er toen waren. het spel zelf detecteerd of er iemand gewonnen heeft. ook laat spel zien wie er aan de beurt is.`,
-                    link: "/software/boter kaas en eieren/index.html",
-},]
-}
+  projecten: [
+    {
+      title: "boter, kaas en eieren",
+      img: "/img/tic tac toe.png",
+      text: `dit is een digitale versie van het spel boter kaas en eieren. het spel is gemaakt in 2022. het spel heeft het thema van de boeren protesten die er toen waren. het spel zelf detecteerd of er iemand gewonnen heeft. ook laat spel zien wie er aan de beurt is.`,
+      link: "/software/boter kaas en eieren/index.html",
+    },
+    {
+      title: "hoger lager",
+      img: "img/hogerlager img.png",
+      text: `Dit is een digitale versie van het drank spel hoger lager. Dit is samen met David Aalderink gemaakt als schoolproject. gemaakt in het jaar 2022.`,
+      link: "/software/hoger lager (2)/hoger lager project/homepage project/index.html",
+    }, {title: "webcatalogus"},
+  ],
+};
